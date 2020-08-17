b872e986005fde73f66328c3bf82d68292b27ff6
Edward Withers <dearshrewdwit@gmail.com>
2020-08-17 13:51:38 +0100

    require files

--- lib/app.rb
+++ lib/app.rb
@@ -1,2 +1,8 @@
 require_relative 'errors'
 require_relative 'sort_command'
+require_relative 'sort_presenter'
+require_relative 'sorts/base'
+require_relative 'sorts/total_visits'
+require_relative 'sorts/unique_visits'
+require_relative 'page_visit'
+require_relative 'log'
